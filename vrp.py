733293e370860947e942f87cde97b069b95e042f
Serenay Akgün <serenayfromturkey@gmail.com>
2024-03-24 14:50:46 +0300

    distance matrix filtering with tasks

--- vrp.py
+++ vrp.py
@@ -3,6 +3,7 @@ import networkx as nx
 import ortools
 import koridor_deneme
 import pandas as pd
+import ast
 
 def distanceMatrixCreate(G):
 
@@ -21,12 +22,36 @@ def distanceMatrixCreate(G):
                 else:
                     distance_matrix[i][j] = float('inf')
 
-    print("Distance Matrix:")
-    print(distance_matrix)
+    #print("Distance Matrix:")
+    #print(distance_matrix)
+    return distance_matrix, nodes
 
 def fixedLocationVRP(distanceMatrix, taskList):
     pass
 
+def convert_string_tuples_to_int_tuples(array_of_string_tuples):
+    # Use ast.literal_eval to safely evaluate the string as a Python expression
+    # Then convert each element to a tuple of integers
+    return [tuple(ast.literal_eval(item)) for item in array_of_string_tuples]
+
+def taskDistanceMatrix(tasks, nodes, distanceMatrix):
+    """Inputs
+    tasks (ndarray): array of tasks from
+    nodes (list): nodes list directly from networkx
+    distanceMatrix (ndarray): created with distanceMatrixCreate
+    
+    Ouput: Distance matrix consisting distances only from task nodes to other task nodes."""
+
+    formatted_tasks = convert_string_tuples_to_int_tuples(tasks.tolist())
+
+    same_count = sum(1 for item in formatted_tasks if item in nodes)
+    print(f"Number of matching items: {same_count}")
+    
+    matching_indexes = [i for i, item in enumerate(nodes) if item in formatted_tasks]
+    print(f"Indexes of matching items in nodes_df1: {matching_indexes}")
+
+    taskDistanceMatrix = distanceMatrix[np.ix_(matching_indexes, matching_indexes)]
+    return taskDistanceMatrix
 
 
 if __name__ == "__main__":
@@ -39,17 +64,21 @@ if __name__ == "__main__":
     koridor_deneme.draw_network_with_shelves(rectangular_network, pos)
     network_corridors = koridor_deneme.create_corridor_subgraph(rectangular_network)
 
-    distMatrix = distanceMatrixCreate(rectangular_network)
+    distMatrix, nodes = distanceMatrixCreate(rectangular_network)
 
     taskdf1 = pd.read_excel("2pickstation-2robot.xlsx", sheet_name="Sim2-East")
     taskdf1 = taskdf1["SimPy Location"]
     task1arr = np.unique(taskdf1.to_numpy())
 
 
-
     taskdf2 = pd.read_excel("2pickstation-2robot.xlsx", sheet_name="Sim2-West")
     taskdf2 = taskdf2["SimPy Location"]
     task2arr = taskdf2.to_numpy()
     stacked_arr = np.concatenate((task1arr, task2arr))
+    print(distMatrix)
+    print(nodes)
     ab=10
 
+
+    bot1DistMatrix = taskDistanceMatrix(task1arr, nodes, distMatrix)
+    print(bot1DistMatrix)
